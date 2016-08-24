20f2646552e47071c25343087115bb3c8ecd5e25
Nellore <krishnakumar.nellore@imaginea.com>
2016-08-24 17:53:23 +0530

    Added Test Case to Verify Sort Filter category and Sorting functionality for Men's fashion By Discount.

--- src/test/java/snapdeal/SnapDealTestFrameWork/FashionTests.java
+++ src/test/java/snapdeal/SnapDealTestFrameWork/FashionTests.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.imaginea.pageobjects.FashionPageActivity;
 import com.imaginea.pageobjects.HomePageActivity;
@@ -39,11 +40,12 @@ public class FashionTests extends BaseTest {
 
 	@Test(description = "Verify Sub Category")
 	public void verifySubCategory() {
+		SoftAssert s_assert = new SoftAssert();
 		HomePageActivity homePage = new HomePageActivity(driver);
 		String category[] = { "Men's Fashion", "Women's Fashion",
 				"Baby & Kids", "Jewellery", "Bags & Luggage",
 				"Sports, Fitness & Outdoor" };
-		String subCategory[] = { "Clothing", "Ethnic Wear", "Toys & Games",
+		String subCategory[] = { "Clothing", "Ethnic Wear", "Kids Clothing",
 				"Fashion Jewellery", "Backpacks & More", "Sports" };
 
 		for (int i = 0; i < category.length; i++) {
@@ -51,10 +53,47 @@ public class FashionTests extends BaseTest {
 			FashionPageActivity fashionPageActivity = new FashionPageActivity(
 					driver);
 			fashionPageActivity.selectSubCategory(category[i]);
-			Assert.assertEquals(UIUtility.getElementTextByIndex(driver, 1),
+			s_assert.assertEquals(UIUtility.getElementTextByIndex(driver, 1),
 					subCategory[i], "Sub Category result field didn't match");
 			driver.navigate().back();
 		}
+		s_assert.assertAll();
+	}
+
+	@Test(description = "Verify List of Sort Type Avilable for Fashion Category")
+	public void verifyMenFashionClothingCategorySortType() {
+		HomePageActivity homePage = new HomePageActivity(driver);
+		homePage.selectCategory("Fashion");
+		FashionPageActivity fashionPageActivity = new FashionPageActivity(
+				driver);
+		fashionPageActivity.selectSubCategory("Men's Fashion");
+		UIUtility.sleep(10000L);
+		UIUtility.clickElementByText(driver, "Clothing");
+		UIUtility.sleep(10000L);
+		UIUtility.clickElementByText(driver, "Sort\nPopularity");
+		String sortCategory[] = { "Relevance", "Popularity",
+				"Price Low To High", "Price High To Low", "New Arrival",
+				"Discount" };
+		Assert.assertEquals(fashionPageActivity.getSortCategoryList(),
+				Arrays.asList(sortCategory));
+	}
+
+	@Test(description = "Verify Sort By discount in Fashion Category")
+	public void verifyMenFashionSortByDiscount() {
+		HomePageActivity homePage = new HomePageActivity(driver);
+		homePage.selectCategory("Fashion");
+		FashionPageActivity fashionPageActivity = new FashionPageActivity(
+				driver);
+		fashionPageActivity.selectSubCategory("Men's Fashion");
+		UIUtility.sleep(5000L);
+		UIUtility.clickElementByText(driver, "Clothing");
+		UIUtility.sleep(5000L);
+		UIUtility.clickElementByText(driver, "Sort\nPopularity");
+		UIUtility.clickElementByText(driver, "Discount");
+		UIUtility.sleep(5000L);
+		Assert.assertTrue(fashionPageActivity.getProductDiscountList().get(0)
+				.contains("% OFF"), "Expected "
+				+ fashionPageActivity.getProductDiscountList().get(0));
 
 	}
 
